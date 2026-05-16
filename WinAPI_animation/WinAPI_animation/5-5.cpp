d588a820cf42137a9772e609c9a97af851309f96
sanu1227 <rlatosn@tukorea.ac.kr>
2026-05-16 22:50:22 +0900

    Feat: add main winapi

---
+++ WinAPI_animation/WinAPI_animation/5-5.cpp
@@ -0,0 +1,74 @@
+#include <windows.h> //--- 윈도우 헤더 파일
+#include <tchar.h>
+
+HINSTANCE g_hInst;
+LPCTSTR lpszClass = L"My Window Class";
+LPCTSTR lpszWindowName = L"Window Programming Lab";
+LRESULT CALLBACK WndProc(HWND hWnd, UINT iMessage, WPARAM wParam, LPARAM lParam);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdParam, int nCmdShow)
+{
+	HWND hWnd;
+	MSG Message;
+	WNDCLASSEX WndClass;
+	g_hInst = hInstance;
+	WndClass.cbSize = sizeof(WndClass);
+	WndClass.style = CS_HREDRAW | CS_VREDRAW;
+	WndClass.lpfnWndProc = (WNDPROC)WndProc;
+	WndClass.cbClsExtra = 0;
+	WndClass.cbWndExtra = 0;
+	WndClass.hInstance = hInstance;
+	WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	WndClass.lpszMenuName = NULL;
+	WndClass.lpszClassName = lpszClass;
+	WndClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
+	RegisterClassEx(&WndClass);
+	hWnd = CreateWindow(lpszClass, lpszWindowName, WS_OVERLAPPEDWINDOW, 0, 0, 800, 600, NULL, (HMENU)NULL, hInstance, NULL);
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	while (GetMessage(&Message, 0, 0, 0)) {
+		TranslateMessage(&Message);
+		DispatchMessage(&Message);
+	}
+	return Message.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hDC, mDC;
+	RECT rt;
+	HBITMAP hBitmap;
+
+
+	//--- 메시지 처리하기
+	switch (uMsg) {
+	case WM_CREATE:
+
+		break;
+	case WM_PAINT:
+		GetClientRect(hWnd, &rt);
+		hDC = BeginPaint(hWnd, &ps);
+		mDC = CreateCompatibleDC(hDC); //--- 메모리 DC 만들기
+		hBitmap = CreateCompatibleBitmap(hDC, rt.right, rt.bottom); //--- 메모리 DC와 연결할 비트맵 만들기
+		SelectObject(mDC, (HBITMAP)hBitmap);
+		FillRect(mDC, &rt, (HBRUSH)GetStockObject(WHITE_BRUSH));
+
+
+		BitBlt(hDC, 0, 0, rt.right, rt.bottom, mDC, 0, 0, SRCCOPY);
+
+		DeleteDC(mDC); //--- 생성한 메모리 DC 삭제
+		DeleteObject(hBitmap);
+		EndPaint(hWnd, &ps);
+		break;
+	case WM_TIMER:
+		InvalidateRect(hWnd, NULL, false);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	}
+	return DefWindowProc(hWnd, uMsg, wParam, lParam); //--- 위의 세 메시지 외의 나머지 메시지는 OS로
+}
